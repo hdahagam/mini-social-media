30433fd0759740375ef9a2f76c83c72f749c212f
Hemanth Dahagam <hdahagam@gmail.com>
2019-06-26 17:28:56 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule,MatToolbarModule, MatButtonModule, MatExpansionModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter,Output} from '@angular/core';
+import {Post} from '../post.model';
+import { NgForm } from '@angular/forms';
+@Component({
+    selector: 'app-post-create',
+    templateUrl:'./post-create.component.html',
+    styleUrls:['./post-create.component.css']
+})
+export class PostCreateComponent{
+    enteredContent="";
+    enteredTitle="";
+    @Output() postCreated = new EventEmitter<Post>();
+
+    onAddPost(form: NgForm){
+        if(form.invalid)
+            return;
+       const post:Post={
+           title:  form.value.title,
+           content: form.value.content      
+        };
+        console.log(post.title+'hi'); 
+        this.postCreated.emit(post);
+    }
+}
